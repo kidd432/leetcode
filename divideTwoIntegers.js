b0437fb566b747f31ba74a67483b5f2b410a75c7
nake12 <nake37@gmail.com>
2017-10-09 21:49:47 -0400

    10092017

---
+++ divideTwoIntegers.js
@@ -0,0 +1,16 @@
+/**
+ * Created by nake12 on 2017/10/9.
+ *
+ *
+ * Divide two integers without using multiplication, division and mod operator.
+
+ If it is overflow, return MAX_INT.
+ */
+/**
+ * @param {number} dividend
+ * @param {number} divisor
+ * @return {number}
+ */
+var divide = function(dividend, divisor) {
+
+};
